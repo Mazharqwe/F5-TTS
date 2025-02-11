1f814c4e4fc9e262a574384baa4052080720bb4d
Jam Mazhar <jammazhar688@gmail.com>
2025-02-11 13:20:30 +0500

    gradio interface top text changes

--- src/f5_tts/infer/infer_gradio.py
+++ src/f5_tts/infer/infer_gradio.py
@@ -747,18 +747,12 @@ Have a conversation with an AI using your reference voice!
 with gr.Blocks() as app:
     gr.Markdown(
         """
-# E2/F5 TTS
-
-This is a local web UI for F5 TTS with advanced batch processing support. This app supports the following TTS models:
-
-* [F5-TTS](https://arxiv.org/abs/2410.06885) (A Fairytaler that Fakes Fluent and Faithful Speech with Flow Matching)
-* [E2 TTS](https://arxiv.org/abs/2406.18009) (Embarrassingly Easy Fully Non-Autoregressive Zero-Shot TTS)
 
 The checkpoints currently support English and Chinese.
 
 If you're having issues, try converting your reference audio to WAV or MP3, clipping it to 15s with  ✂  in the bottom right corner (otherwise might have non-optimal auto-trimmed result).
 
-**NOTE: Reference text will be automatically transcribed with Whisper if not provided. For best results, keep your reference clips short (<15s). Ensure the audio is fully uploaded before generating.**
+**NOTE:  For best results, keep your reference clips short (<15s). Ensure the audio is fully uploaded before generating.**
 """
     )
 
