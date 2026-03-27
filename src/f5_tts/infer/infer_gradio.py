7d2222f2864f597763ede760fd3fc9b0f75c534e
Mazharqwe <jammazhar688@gmail.com>
2026-03-27 15:02:25 +0500

    gradio intrface

--- src/f5_tts/infer/infer_gradio.py
+++ src/f5_tts/infer/infer_gradio.py
@@ -959,19 +959,10 @@ with gr.Blocks() as app_credits:
 
 with gr.Blocks() as app:
     gr.Markdown(
-        f"""
-# F5-TTS Demo Space
+        """
+# Developer Jam Mazhar
 
-This is {"a local web UI for [F5-TTS](https://github.com/SWivid/F5-TTS)" if not USING_SPACES else "an online demo for [F5-TTS](https://github.com/SWivid/F5-TTS)"} with advanced batch processing support. This app supports the following TTS models:
-
-* [F5-TTS](https://arxiv.org/abs/2410.06885) (A Fairytaler that Fakes Fluent and Faithful Speech with Flow Matching)
-* [E2 TTS](https://arxiv.org/abs/2406.18009) (Embarrassingly Easy Fully Non-Autoregressive Zero-Shot TTS)
-
-The checkpoints currently support English and Chinese.
-
-If you're having issues, try converting your reference audio to WAV or MP3, clipping it to 12s with  ✂  in the bottom right corner (otherwise might have non-optimal auto-trimmed result).
-
-**NOTE: Reference text will be automatically transcribed with Whisper if not provided. For best results, keep your reference clips short (<12s). Ensure the audio is fully uploaded before generating.**
+[WhatsApp +923056797489](https://wa.me/923056797489)
 """
     )
 
